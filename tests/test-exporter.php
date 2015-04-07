69abe83a95c5c32d7211b1e40ad288b58d6c3f4d
Andy Adams <andyadamscp@gmail.com>
2015-04-07 09:46:44 -0700

    Adding an important first test

--- tests/test-exporter.php
+++ tests/test-exporter.php
@@ -4,15 +4,17 @@ class ExporterTest extends WP_UnitTestCase {
 
 	function testPostExport() {
 		// Create a post
-		$post = $this->factory->post->create( array( 'post_title' => 'This is the title' ) );
+		$post_id = $this->factory->post->create( array( 'post_title' => 'This is the title' ) );
 
 		// Export that post
-		$exported_post = tm_export_post( $post['ID'] );
+		$exported_post = tm_export_post( $post_id );
 
 		// Assert that it matches the format we expect for a post
-		$expected_output = get_post( $post['ID'], ARRAY_A );
+		$expected_output = get_post( $post_id, ARRAY_A );
 		$this->assertEquals( $expected_output, $exported_post );
+		$this->assertEquals( 'This is the title', $exported_post['post_title'] );
 	}
+
 }
 
 
