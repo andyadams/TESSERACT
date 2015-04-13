5c03c58aa1d1ca4f6d327e896c31395ac1e72249
Andy Adams <andyadamscp@gmail.com>
2015-04-13 11:37:20 -0700

    Adding some styles to importer landing page

--- importer/scripts.php
+++ importer/scripts.php
@@ -5,6 +5,7 @@ function tesseract_enqueue_importer_scripts() {
 
 	if ( 'admin.php' == $pagenow && isset( $_GET['page'] ) && 'tesseract-importer' == $_GET['page'] ) {
 		wp_enqueue_script( 'tesseract-importer-admin', get_template_directory_uri() . '/importer/js/importer-admin.js', array( 'jquery' ) );
+		wp_enqueue_style( 'tesseract-importer-admin', get_template_directory_uri() . '/importer/css/admin.css' );
 	}
 }
 
--- importer/templates/partials/_package-display.php
+++ importer/templates/partials/_package-display.php
@@ -1,30 +1,35 @@
 <div class="package">
 	<form action="<?php echo esc_url( tesseract_get_import_package_url() ); ?>" method="post" class="package-form" data-num-pages="<?php echo esc_attr( count( $package['posts'] ) ); ?>">
-		<?php if ( ! empty( $package['image'] ) ) : ?>
-			<div class="image">
+		<div class="image">
+			<?php if ( ! empty( $package['image'] ) ) : ?>
 				<img src="<?php echo esc_url( $package['image'] ); ?>">
-			</div>
-		<?php endif; ?>
-		<h4 class="name">
-			<?php echo esc_html( $package['name'] ); ?>
-		</h4>
-		<?php if ( ! empty( $package['details']['description'] ) ) : ?>
-			<div class="description">
-				<?php
-					echo wp_kses( $package['details']['description'], array(
-						'a' => array(
-							'href' => array(),
-							'title' => array()
-						),
-						'br' => array(),
-						'em' => array(),
-						'strong' => array(),
-					) );
-				?>
-			</div>
-		<?php endif; ?>
-		<input type="hidden" name="package" value="<?php echo esc_attr( $package['id'] ); ?>">
-		<input type="submit" class="button button-secondary" value="Import This Package">
+			<?php else : ?>
+				<span class="image-placeholder"><?php echo esc_html( $package['name'] ); ?></span>
+			<?php endif; ?>
+		</div>
+		<div class="content">
+			<h4 class="name">
+				<?php echo esc_html( $package['name'] ); ?>
+			</h4>
+			<?php if ( ! empty( $package['details']['description'] ) ) : ?>
+				<div class="description">
+					<?php
+						echo wp_kses( $package['details']['description'], array(
+							'a' => array(
+								'href' => array(),
+								'title' => array()
+							),
+							'br' => array(),
+							'em' => array(),
+							'strong' => array(),
+						) );
+					?>
+				</div>
+			<?php endif; ?>
+			<input type="submit" class="button button-secondary" value="Import This Package">
+		</div>
+
 		<?php wp_nonce_field( 'tesseract_import_package' ); ?>
+		<input type="hidden" name="package" value="<?php echo esc_attr( $package['id'] ); ?>">
 	</form>
 </div>
