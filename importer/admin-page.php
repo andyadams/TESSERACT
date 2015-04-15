0a6b4c3efec326a8bc8771ae22d93a7551ef0faa
Andy Adams <andyadamscp@gmail.com>
2015-04-15 10:33:41 -0700

    Cleaning up import code a smidge

--- importer/admin-page.php
+++ importer/admin-page.php
@@ -36,6 +36,9 @@ function tesseract_handle_package_import() {
 		if ( is_wp_error( $result ) ) {
 			tesseract_add_error_message( "Error: Package import was incomplete: " . $result->get_error_message() );
 		} else {
+			// After a package is imported, we redirect the user to the results page to prevent re-importing on page refresh.
+			// We have to store the import result temporarily, until the next request.
+			// See tesseract_load_import_result() for what happens after import.
 			update_option( 'tesseract_import_result', $result );
 			wp_redirect( tesseract_get_import_package_url() );
 			exit;
@@ -43,23 +46,29 @@ function tesseract_handle_package_import() {
 	}
 }
 
-add_action( 'admin_init', 'tesseract_handle_package_import' );
-
+/**
+ * If a package has been successfully imported, we need to display the results of the import.
+ */
 function tesseract_load_import_result() {
+	// Are we on the import package page?
 	if ( tesseract_is_import_package_page() ) {
 		$result = get_option( 'tesseract_import_result' );
 
+		// If so, and we have the results of an import, we set that up for display here.
 		if ( ! empty( $result ) ) {
 			global $tesseract_import_result;
 			$tesseract_import_result = $result;
 			tesseract_add_success_message( "Success! Imported '{$result['name']}' Package." );
 			delete_option( 'tesseract_import_result' );
+		} elseif ( tesseract_is_valid_package_import() ) {
+			// If this is an import (with POST data), do the import!
+			tesseract_handle_package_import();
 		} else {
-			if ( ! tesseract_is_valid_package_import() ) {
-				wp_redirect( tesseract_get_import_home_url() );
-				exit;
-			}
+			// Otherwise, if this isn't a package import and we don't have a result, we shouldn't be here...redirect!
+			wp_redirect( tesseract_get_import_home_url() );
+			exit;
 		}
+
 	}
 }
 
